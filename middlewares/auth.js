7a5afbc324556c966c4d7ae6fa4208c5954b59e1
Habid B Espinosa <hab.bad@hotmail.com>
2021-10-25 13:46:26 +0200

    backend setup, middleware auth added and models: User, Project, Job added seeds file added

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+import createError from "http-errors"
+// import User from "../../models/User.js";
+
+const auth = async (req, res, next) => {
+    
+    try {
+        // grab the token from the cookie
+        // console.log("REQ COOKIES =>", req.cookies);
+        const token = req.cookies.token
+    
+        // verify token
+        const user = await User.findByToken(token)
+        if(!user) next(createError(401, `Auth failed. Please try to login again!!`))
+    
+        // if a user exist, pass the user in the req for future use
+        req.user = user
+
+        next()
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default auth
