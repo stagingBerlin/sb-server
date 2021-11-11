6fdae12614b259d49895d35648ceba2c8688c9aa
Habid B Espinosa <hab.bad@hotmail.com>
2021-11-11 09:05:54 +0100

    project routes updated

--- middlewares/projectMiddlewares/isOwner.js
+++ middlewares/projectMiddlewares/isOwner.js
@@ -5,6 +5,12 @@ const isOwner = async (req, res, next) => {
     const id = req.params.id;
     try {
         const projectdb = await Project.findById(id)
+        // .populate('owner')
+        // .populate("jobList")
+        // .populate("participants");
+
+        console.log(projectdb.owner._id);
+        console.log(req.user._id);
         // if the user is not the owner of the project, we throw an error
         if(projectdb.owner !== req.user._id) next(createError(401, `this user is not the owner of this project!!`));
 
