4b172a73b3c39a0c4c3cb7ad91da4db9cc3cd28a
Habid B Espinosa <hab.bad@hotmail.com>
2021-11-11 12:02:10 +0100

    ownProjects route updated

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -15,23 +15,23 @@ import auth from '../middlewares/authentication/auth.js'
 import isAdmin from '../middlewares/authentication/isAdmin.js'
 import isOwner from '../middlewares/projectMiddlewares/isOwner.js'
 
-
 router.route('/')
 .get(auth, getAllProjects)
 .post(auth, createProject);
 
 router.route('/ownProjects')
-.get(auth, getOwnProjects);
+.get(auth, isOwner, getOwnProjects);
 
 router.route('/:id')
 .get(auth, getProject)
 .delete(auth, isAdmin, deleteProject);
 
+// route to access to the detailes of each owned Project, update and delete also posible just by the owner.
 router.route('/ownProjects/:id')
 .get(auth, isOwner, getOwnProject)
 .put(auth, isOwner, updateOwnProject)
 .delete(auth, isOwner, deleteOwnProject);
 
-
+// still missing a method to fullfield the job list when an owner creates or adds participants to the project
 
 export default router;
