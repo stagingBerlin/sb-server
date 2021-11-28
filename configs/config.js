0e05ca093ea2bc15a9bd56637731f8c825d88d9f
Habid B Espinosa <hab.bad@hotmail.com>
2021-11-28 11:04:12 +0100

    somechanges

--- configs/config.js
+++ configs/config.js
@@ -12,7 +12,6 @@ import { fileURLToPath } from 'url';
 // console.log('import.meta.url', import.meta.url);
 // console.log('fileURLToPath(import.meta.url)', fileURLToPath(import.meta.url));
 
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // console.log('__DIRNAME', __dirname);
